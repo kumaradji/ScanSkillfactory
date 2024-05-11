b65b21435e00ff377497d6fa6025bbd5123e170a
kumaradji <kumaradji@gmail.com>
2024-05-11 12:36:14 +0300

    DateInput.module.scss

--- src/pages/SearchPage/DateInput/DateInput.jsx
+++ src/pages/SearchPage/DateInput/DateInput.jsx
@@ -2,7 +2,7 @@
 import React, {useEffect, useState} from 'react';
 import styles from './DateInput.module.scss';
 
-const DateInput = ({ startDate, setStartDate, endDate, setEndDate }) => {
+const DateInput = ({startDate, setStartDate, endDate, setEndDate}) => {
   const [error, setError] = useState('');
   const [inputTypeStart, setInputTypeStart] = useState('text');
   const [inputTypeEnd, setInputTypeEnd] = useState('text');
@@ -28,39 +28,47 @@ const DateInput = ({ startDate, setStartDate, endDate, setEndDate }) => {
 
   return (
     <div className={styles.formField}>
-      <label htmlFor="startDate">Диапазон поиска
-        <span className={error ? `${styles.requiredAsterisk} ${styles.error}` : styles.requiredAsterisk}>*</span>
+      <label htmlFor="startDate">Диапазон поиска<span
+        className={error ? `${styles.requiredAsterisk} ${styles.error}` : styles.requiredAsterisk}>*</span>
       </label>
       <div className={styles.formFieldDateInputs}>
         <div className={styles.dateInputContainer}>
-          <input
-            type={inputTypeStart}
-            onFocus={() => setInputTypeStart('date')}
-            onBlur={() => {
-              validateDateRange();
-              if (!startDate) setInputTypeStart('text');
-            }}
-            id="startDate"
-            name="startDate"
-            placeholder="Дата начала"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-            className={error ? styles.error : ''}
-          />
-          <input
-            type={inputTypeEnd}
-            onFocus={() => setInputTypeEnd('date')}
-            onBlur={() => {
-              validateDateRange();
-              if (!endDate) setInputTypeEnd('text');
-            }}
-            id="endDate"
-            name="endDate"
-            placeholder="Дата конца"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-            className={error ? styles.error : ''}
-          />
+          <div className={styles.dateInputWrapper}>
+            <input
+              type={inputTypeStart}
+              onFocus={() => setInputTypeStart('date')}
+              onBlur={() => {
+                validateDateRange();
+                if (!startDate) setInputTypeStart('text');
+              }}
+              id="startDate"
+              name="startDate"
+              placeholder="Дата начала"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+              className={error ? styles.error : ''}
+            />
+            <span className={styles.dateIcon}></span>
+          </div>
+
+          <div className={styles.dateInputWrapper}>
+            <input
+              type={inputTypeEnd}
+              onFocus={() => setInputTypeEnd('date')}
+              onBlur={() => {
+                validateDateRange();
+                if (!endDate) setInputTypeEnd('text');
+              }}
+              id="endDate"
+              name="endDate"
+              placeholder="Дата конца"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              className={error ? styles.error : ''}
+            />
+            <span className={styles.dateIcon}></span>
+          </div>
+
         </div>
         {error && <div className={`${styles.dateErrorMessage} ${styles.error}`}>{error}</div>}
       </div>
